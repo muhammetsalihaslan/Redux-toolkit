3778d56efd2eedeef5904b11914db0ebb4b6d096
salih <muhammetsalihaslan@gmail.com>
2022-08-01 20:46:50 +0300

    cont3

---
+++ redux-toolkit-example/src/features/newsSlice.jsx
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+    user: '',
+};
+
+
+const Slice = createSlice({
+   name: 'news',
+   initialState,
+   reducers: {
+    
+
+   },
+});
+
+// export const { setUser, clearUser } = authSlice.actions;
+
+export default newsSlice.reducer;
